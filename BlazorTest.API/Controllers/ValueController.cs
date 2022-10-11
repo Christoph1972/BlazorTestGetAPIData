288f5f6f2514122e8e294cd3689389bbca3f67ec
Christoph1972 <christoph_1972@arcor.de>
2022-10-11 21:43:04 +0200

    It's working now. :-)

---
+++ BlazorTest.API/Controllers/ValueController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        // GET: api/Value
+        [HttpGet]
+        public string Get()
+        {
+            return "Just a value";
+        }
+
+    }
+}
--- BlazorTest.API/Program.cs
+++ BlazorTest.API/Program.cs
@@ -1,19 +1,23 @@
 using Microsoft.Net.Http.Headers;
 
-
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 //https://localhost:7096;http://localhost:5096 Webpage urls
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("https://localhost:7096;http://localhost:5096");
-                      });
+    /*
+    Browser security prevents a web page from making requests to a different domain than the one that served 
+    the web page. This restriction is called the same-origin policy. The same-origin policy prevents a malicious 
+    site from reading sensitive data from another site. Sometimes, you might want to allow other sites to make 
+    cross-origin requests to your app. For more information, see the Mozilla CORS article.
+     */
+    options.AddPolicy(MyAllowSpecificOrigins, builder => builder.WithOrigins("https://localhost:7096")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowed((host) => true));
 });
 
 builder.Services.AddControllers();
@@ -32,15 +36,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-app.UseRouting();
 app.UseCors(MyAllowSpecificOrigins);
-
-//https://localhost:7096;http://localhost:5096 Webpage urls
-//app.UseCors(policy =>
-//    policy.WithOrigins("https://localhost:7096;http://localhost:5096")
-//    .AllowAnyMethod()
-//    .WithHeaders(HeaderNames.ContentType));
 
 app.UseAuthorization();
 
